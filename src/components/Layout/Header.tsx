89425646603dac158e1ed4abb22a4eb1e0e7cfc4
Daniel Trott <d.trott87@gmail.com>
2022-07-15 16:45:13 +0200

    Some test

---
+++ src/components/Layout/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type HeaderProps = {}
+
+const Header = (props: HeaderProps) => {
+  return (
+    <div id="header">
+        
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+
+type Props = {
+  children: ReactNode[];
+};
+
+const Layout = (props: Props) => {
+  return (
+    <>
+      <Header />
+      <main>{props.children}</main>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
