c2de7ddd8d8b942d7027f9e0bbb8662b9b52624a
Daniel Trott <d.trott87@gmail.com>
2022-07-15 16:49:06 +0200

    Added missing ReactNode import

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 
 type Props = {
-  children: ReactNode[];
+  children: ReactNode;
 };
 
 const Layout = (props: Props) => {
