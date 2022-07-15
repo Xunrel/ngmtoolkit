056026fb32fa4b37319fac73d66945a6eb067600
Daniel Trott <d.trott87@gmail.com>
2022-07-15 16:58:03 +0200

    emoji test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 
 const Home: NextPage = () => {
-  const k = "kilian mama"
+  const k = "kilian mama".split(" ").join("😁😁😁");
   return (
     <>
       <Head>
@@ -13,9 +13,7 @@ const Home: NextPage = () => {
 
       <div className="w-screen h-screen flex flex-col justify-center items-center p-4">
         <h2 className="text-[3rem] lg:text-[5rem] md:text-[5rem] font-extrabold text-gray-700">
-          <span className="text-red-400">
-          {k.toUpperCase()}
-          </span>
+          <span className="text-red-400">{k.toUpperCase()}</span>
         </h2>
         <p className="text-2xl text-gray-700">This stack uses</p>
         <div className="grid grid-cols-1 grid-rows-3 lg:grid-rows-1 md:grid-rows-1 lg:grid-cols-3 md:grid-cols-3 gap-3 mt-3 pt-3 w-full lg:w-2/3 md:w-full">
